ae19035e69af5fe7fba0c6afd99e35a40a25be40
supergrubby <supergrubby@foxmail.com>
2014-04-09 10:00:22 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
-
 	<div id="footer">
-		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
-		<span class="meta-sep">|</span>
-		<span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
 	</div><!-- #footer -->
 
 </div><!-- #wrapper .hfeed -->
 
 <?php wp_footer() ?>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Ffc03a4e5ea923ba337983cc5434c0ce7' type='text/javascript'%3E%3C/script%3E"));
+</script>
 
 </body>
 </html>
